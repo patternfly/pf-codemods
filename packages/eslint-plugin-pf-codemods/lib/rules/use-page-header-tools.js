650b27b38b73a9198a41257ead8ff89426ef44fc
jenny-s51 <jshandel12@gmail.com>
2020-05-22 13:15:19 -0400

    targeting JSXIdentifiers

--- packages/eslint-plugin-pf-codemods/lib/rules/use-page-header-tools.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/use-page-header-tools.js
@@ -1,10 +1,28 @@
-const { renameProp } = require('../helpers');
+// const { renameProp } = require('../helpers');
+const { getPackageImports } = require('../helpers');
 
+const renames = {
+  'Toolbar': 'PageHeaderTools',
+  'ToolbarGroup': 'PageHeaderToolsGroup',
+  'ToolbarItem': 'PageHeaderToolsItem'
+};
 module.exports = {
-  create: renameProp(
-    'PageHeader',
-    { 'avatar={<Avatar  />}': 'headerTools={<PageHeaderTools> <Avatar  /> </PageHeaderTools>}', 'toolbar': 'headerTools={<PageHeaderTools  />}' },
-    node => `avatar and toolbar props were removed for ${node.name.name}. use headerTools prop instead.`,
-    true
-  )
+  create: function(context) {
+    const imports = getPackageImports(context, '@patternfly/react-core')
+      .filter(specifier => Object.keys(renames).includes(specifier.imported.name));
+    return !imports ? {} : {
+      JSXIdentifier(node) {
+        if (imports.map(imp => imp.local.name).includes(node.name)) {
+          const isOpeningToolbar = node.parent.type === 'JSXOpeningElement' && node.name === 'Toolbar';
+          context.report({
+            node,
+            message: `${node.name} renamed to ${renames[node.name]}`,
+            fix(fixer) {
+              return fixer.replaceText(node, `${renames[node.name]}${isOpeningToolbar ? ' data-codemods="true"' : ''}`);
+            }
+          });
+        }
+      }
+    };
+  }
 };
