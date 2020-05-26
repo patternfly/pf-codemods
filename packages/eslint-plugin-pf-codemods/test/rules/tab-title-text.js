5076f77f74208c4df81b0fee50c48844a9302955
Joachim Schuler <jschuler@redhat.com>
2020-05-26 11:43:48 -0400

    rule test

--- packages/eslint-plugin-pf-codemods/test/rules/tab-title-text.js
+++ packages/eslint-plugin-pf-codemods/test/rules/tab-title-text.js
@@ -4,7 +4,7 @@ const rule = require('../../lib/rules/tab-title-text');
 ruleTester.run("tab-title-text", rule, {
   valid: [
     {
-      code: `import { Tab } from '@patternfly/react-core'; <Tab title={<TabTitleText>Title</TabTitleText>}>Content</Tab>`,
+      code: `import { Tab, TabTitleText } from '@patternfly/react-core'; <Tab title={<TabTitleText>Title</TabTitleText>}>Content</Tab>`,
     },
     {
       // No @patternfly/react-core import
@@ -14,7 +14,7 @@ ruleTester.run("tab-title-text", rule, {
   invalid: [
     {
       code:   `import { Tab } from '@patternfly/react-core'; <Tab title="Title">Content</Tab>`,
-      output: `import { Tab } from '@patternfly/react-core'; <Tab title={<TabTitleText>Title</TabTitleText>}>Content</Tab>`,
+      output: `import { Tab, TabTitleText } from '@patternfly/react-core'; <Tab title={<TabTitleText>Title</TabTitleText>}>Content</Tab>`,
       errors: [{
         message: `title needs to be wrapped with the TabTitleText and/or TabTitleIcon component`,
         type: "JSXOpeningElement",
