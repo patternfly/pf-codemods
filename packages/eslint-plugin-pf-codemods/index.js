e7f15276b79e47598f8ea41b3ca5099b171bd427
redallen <zallen@redhat.com>
2020-05-28 11:44:16 -0400

    add form-fix-isValid

--- packages/eslint-plugin-pf-codemods/index.js
+++ packages/eslint-plugin-pf-codemods/index.js
@@ -33,6 +33,7 @@ const rules = {
   "tab-title-text": require('./lib/rules/tab-title-text'),
   "table-removed-transforms": require('./lib/rules/table-removed-transforms'),
   "select-rename-checkbox": require('./lib/rules/select-rename-checkbox'),
+  "form-fix-isValid": require('./lib/rules/form-fix-isValid'),
 };
 
 module.exports = {
