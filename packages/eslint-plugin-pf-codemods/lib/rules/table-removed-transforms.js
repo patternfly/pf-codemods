fcaebd6f0d4c1b5ae4655efed44e6718e9c2465b
evwilkin <evan.wilkinson@gmail.com>
2020-05-29 10:40:22 -0400

    updating code to detect comma before/after

--- packages/eslint-plugin-pf-codemods/lib/rules/table-removed-transforms.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/table-removed-transforms.js
@@ -8,11 +8,150 @@ module.exports = {
     if (tableImports.length) {
       const cellWidthImport = tableImports.filter(specifier => specifier.imported.name === 'cellWidth');
       const cellHeightAutoImport = tableImports.filter(specifier => specifier.imported.name === 'cellHeightAuto');
+      
+      return (!cellWidthImport.length && !cellHeightAutoImport.length) ? {} : {
+        CallExpression(node) {
+          // console.log('node: ', node.callee.name);
+          //////// cellWidth('max')
+          const maxNode = node.callee.name && 
+            node.callee.name === 'cellWidth' &&
+            node.arguments.find(arg => arg.value === 'max');
+          if (maxNode) {
+			context.report({
+              node, 
+              message: `cellWidth('max') has been replaced with cellWidth(100)`, 
+              fix(fixer) {
+                return fixer.replaceText(maxNode, '100');
+              }
+            });
+          }
+
+          ////////// end cellWidth('max')
+          ////////// cellHeightAuto() 
+          // console.log(node); 
+          const isCellHeightAutoNode = node.callee.name && 
+            node.callee.name === 'cellHeightAuto';
+          if (isCellHeightAutoNode) {
+            const sourceCode = context.getSourceCode();
+            sourceCode.getTokens(node).forEach(token => {
+              // console.log('currentToken: ',token.value);
+              let removeFollowingComma = false;
+              let aliasRemoveFollowingComma = false;
+              let hasImportAlias = false; // i.e: NavVariants as MyThing 
+              let removePreviousComma = false;
+              const tokenText = sourceCode.getText(token);
+              const previousToken = sourceCode.getTokenBefore(token);
+              const previousTokenText = sourceCode.getText(previousToken);
+              const nextToken = sourceCode.getTokenAfter(token);
+              const nextTokenText = sourceCode.getText(nextToken);
+              // console.log('nextToken: ',sourceCode.getText(nextToken))
+              // console.log('previousToken: ',sourceCode.getText(previousToken));
+              if (nextTokenText === ',') {
+                removeFollowingComma = true;
+              } else if (nextTokenText === '}' && previousTokenText === ',') {
+                removePreviousComma = true;
+              } else if (nextTokenText === 'as') {
+                hasImportAlias = true;
+                const whatFollowsAlias = sourceCode.getTokensAfter(token, {
+                  count: 3
+                });
+                if (sourceCode.getText(whatFollowsAlias[whatFollowsAlias.length - 1]) === ',') {
+                  aliasRemoveFollowingComma = true;
+                } else if (sourceCode.getText(whatFollowsAlias[whatFollowsAlias.length - 1]) === '}' && previousTokenText === ',') {
+                  removePreviousComma = true;
+                }
+              }
+              context.report({
+                node, 
+                message: `cellHeightAuto transformer has been deprecated, removed usage`, 
+                fix(fixer) {
+                  const fixes = [
+                    fixer.remove(token)
+                  ];
+                  // console.log('remove1: ', token);
+                  if (removePreviousComma) {
+                    fixes.push(fixer.remove(previousToken));
+                    // console.log('remove2: ', previousToken);
+                  }
+                  if (hasImportAlias) {
+                    let additionalTokensToRemove;
+                    if (aliasRemoveFollowingComma) {
+                      // We have something like `NavVariants as MyVariants,` with a comma at the end 
+                      additionalTokensToRemove = sourceCode.getTokensAfter(token, {
+                        count: 3
+                      });
+                    } else {
+                      // Without a comma
+                      additionalTokensToRemove = sourceCode.getTokensAfter(token, {
+                        count: 2
+                      });
+                    }
+                    additionalTokensToRemove.forEach(additionalToken => fixes.push(fixer.remove(additionalToken)));
+                    // console.log('remove4: ', additionalToken);
+                  } else if (removeFollowingComma) {
+                    fixes.push(fixer.remove(nextToken));
+                    // console.log('remove5: ', nextToken);
+                  }
+                  return fixes;
+                }
+              });
+            });
+          }
+          //////////// end cellHeightAuto()
+        }
+      }
+    }
+  }
+};
+
+
+
+
+/*
+module.exports = {
+  create: function(context) {
+    const tableImports = getPackageImports(context, '@patternfly/react-table');
+    
+    if (tableImports.length) {
+      const cellWidthImport = tableImports.filter(specifier => specifier.imported.name === 'cellWidth');
+      const cellHeightAutoImport = tableImports.filter(specifier => specifier.imported.name === 'cellHeightAuto');
       const hasCellWidthImport = cellWidthImport.length;
       const hasCellHeightAutoImport = cellHeightAutoImport.length;
 
       return !(hasCellWidthImport || hasCellHeightAutoImport) ? {} : {
-        Property(node) {
+        CallExpression(node) {
+          //////// cellWidth('max')
+          const maxNode = node.callee.name && 
+            node.callee.name === 'cellWidth' &&
+            node.arguments.find(arg => arg.value === 'max');
+          if (!maxNode) {
+            return;
+          }
+          context.report({
+            node, 
+            message: `cellWidth('max') has been replaced with cellWidth(100)`, 
+            fix(fixer) {
+              return fixer.replaceText(maxNode, '100');
+            }
+          });
+          ////////// end cellWidth('max')
+          ////////// cellHeightAuto()
+          const cellHeightAutoNode = node.callee.name && 
+            node.callee.name === 'cellHeightAuto';
+          if (!cellHeightAutoNode) {
+            return;
+          }
+          context.report({
+            node, 
+            message: `cellWidth('max') has been replaced with cellWidth(100)`, 
+            fix(fixer) {
+              return fixer.remove(node);
+            }
+          });
+          //////////// end cellHeightAuto()
+        },
+
+        CallExpression(node) {
           const transforms = node.key.name === 'transforms' &&
             node.value.type === 'ArrayExpression' &&
             node;
@@ -86,4 +225,4 @@ module.exports = {
     }
   }
 };
-
+*/
--- packages/eslint-plugin-pf-codemods/test/rules/table-removed-transforms.js
+++ packages/eslint-plugin-pf-codemods/test/rules/table-removed-transforms.js
@@ -4,66 +4,34 @@ const rule = require('../../lib/rules/table-removed-transforms');
 ruleTester.run("table-removed-transforms", rule, {
   valid: [
     {
-      code: `
-        import { Table, TableHeader, TableBody, cellWidth } from '@patternfly/react-table';
-        <Table rows={['Row 1']} cells={[{
-          title: 'Last Commit',
-          transforms: [cellWidth(100)]
-        }]}>
-          <TableHeader />
-          <TableBody />
-        </Table>
-      `,
+      code: `import { Table, cellWidth } from "@patternfly/react-table";
+        <Table cells={[{ transforms: [cellWidth(100)] }]}></Table>`,
     }
   ],
   invalid: [
     {
-      code: `import { Table, TableHeader, TableBody, cellWidth } from '@patternfly/react-table';
-        <Table rows={['Row 2']} cells={[{
-          title: 'Last Commit',
-          transforms: [cellWidth('max')]
-        }]}>
-          <TableHeader />
-          <TableBody />
-        </Table>`,
-      output: `import { Table, TableHeader, TableBody, cellWidth } from '@patternfly/react-table';
-        <Table rows={['Row 2']} cells={[{
-          title: 'Last Commit',
-          transforms: [cellWidth(100)]
-        }]}>
-          <TableHeader />
-          <TableBody />
-        </Table>`,
+      code: `import { Table, cellWidth } from "@patternfly/react-table";
+        <Table cells={[{ transforms: [cellWidth('max')] }]}></Table>`,
+      output: `import { Table, cellWidth } from "@patternfly/react-table";
+        <Table cells={[{ transforms: [cellWidth(100)] }]}></Table>`,
       errors: [{
         message: `cellWidth('max') has been replaced with cellWidth(100)`,
-        type: "Property",
+        type: "CallExpression",
       }]
     },
     {
-      code: `import { Table, TableHeader, TableBody, cellWidth, cellHeightAuto } from '@patternfly/react-table';
-        <Table rows={['Row 3']} cells={[{
-          title: 'Last Commit',
-          transforms: [cellWidth('max'), cellHeightAuto()]
-        }]}>
-          <TableHeader />
-          <TableBody />
-        </Table>`,
-      output: `import { Table, TableHeader, TableBody, cellWidth } from '@patternfly/react-table';
-        <Table rows={['Row 3']} cells={[{
-          title: 'Last Commit',
-          transforms: [cellWidth(100)]
-        }]}>
-          <TableHeader />
-          <TableBody />
-        </Table>`,
+      code: `import { Table, cellWidth, cellHeightAuto } from '@patternfly/react-table';
+        <Table cells={[{ transforms: [ cellWidth('max'), cellHeightAuto() ] }]}></Table>`,
+      output: `import { Table, TableHeader, TableBody, cellWidth, cellHeightAuto } from '@patternfly/react-table';
+        <Table cells={[{ transforms: [cellWidth(100)] }]}></Table>`,
       errors: [
-        {
-          message: `cellHeightAuto transformer has been deprecated, import removed`,
-          type: "Property"
-        },
+        // {
+        //   message: `cellHeightAuto transformer has been deprecated, import removed`,
+        //   type: "CallExpression"
+        // },
         {
           message: `cellHeightAuto transformer has been deprecated, removed usage`,
-          type: "Property"
+          type: "CallExpression"
         },
         {
           message: `cellWidth('max') has been replaced with cellWidth(100)`,
