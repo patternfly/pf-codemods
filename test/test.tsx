dc7d533d8572969bbe5539948a69b750c2c8afa2
Evan <evan.wilkinson@gmail.com>
2020-05-29 12:54:02 -0400

    [WIP] Remove cellHeightAuto usage and remove import (#60)
    * updated rule to fix cellHeightAuto usage and remove import
    
    * added test
    
    * 2/3 tests passing
    
    * updating code to detect comma before/after
    
    * cellHeightAuto case
    
    * resolved cellHeightAuto usage
    
    * updated cellHeightAuto function call rule, added cellHeightAuto import removal to remove-unused-imports rule
    
    * removed comments
    
    * PR feedback - clean up if statements
    
    Co-authored-by: Joachim Schuler <jschuler@redhat.com>

--- test/test.tsx
+++ test/test.tsx
@@ -34,3 +34,10 @@ export const TestTab3a = <Tab title={5}>Content</Tab>;
 export const TestTab4 = <Tab title={tabText}>Content</Tab>;
 export const TestTab5 = <Tab title={<TabTextComp />}>Content</Tab>;
 export const TestTab6 = <Tab title={<div>hello</div>}>Content</Tab>;
+
+import { Table, cellWidth } from "@patternfly/react-table";
+export const TableA = <Table cells={[{ transforms: [cellWidth('max')] }]}></Table>;
+
+import { Table, cellWidth, cellHeightAuto } from '@patternfly/react-table';
+export const TableB = <Table cells={[{ transforms: [ cellWidth('max'), cellHeightAuto() ] }]}></Table>
+export const TableC = <Table cells={[{ transforms: [ cellHeightAuto() ] }]}></Table>
