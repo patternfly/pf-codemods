2c8eafe980805a8b4ba89d43a0ed86eab7d3788f
evwilkin <evan.wilkinson@gmail.com>
2020-06-01 21:53:10 -0400

    added Page rule

--- packages/eslint-plugin-pf-codemods/lib/rules/refactor-breakpointmods.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/refactor-breakpointmods.js
@@ -76,3 +76,34 @@ const breakpointProps = {
     }
   }
 }
+
+
+
+visibility: 'hidden' | 'visible';
+
+widths: 'width_25' | 'width_33' | 'width_50' | 'width_66' | 'width_75' | 'width_100';
+
+padding: 'padding' | 'noPadding';
+
+inset: 'insetNone' | 'insetSm' | 'insetMd' | 'insetLg' | 'insetXl' | 'inset2xl';
+
+alignment: 'alignRight' | 'alignLeft';
+
+spacer?: 'spacerNone' | 'spacerXs' | 'spacerSm' | 'spacerMd' | 'spacerLg' | 'spacerXl' | 'spacer2xl' | 'spacer3xl' | 'spacer4xl';
+
+spaceItems?: 'spaceItemsNone' | 'spaceItemsXs' | 'spaceItemsSm' | 'spaceItemsMd' | 'spaceItemsLg' | 'spaceItemsXl' | 'spaceItems2xl' | 'spaceItems3xl' | 'spaceItems4xl';
+
+show: 'show';
+
+grow: 'grow';
+
+shrink: shrink;
+
+flex: 'flexDefault' | 'flexNone' | 'flex_1' | 'flex_2' | 'flex_3' | 'flex_4';
+
+direction: 'column' | 'columnReverse' | 'row' | 'rowReverse';
+
+alignItems: 'alignItemsFlexStart' | 'alignItemsFlexEnd' | 'alignItemsCenter' | 'alignItemsStretch' | 'alignItemsBaseline';
+
+alignContent: 'alignContentFlexStart' | 'alignContentFlexEnd' | 'alignContentCenter' | 'alignContentStretch' | 'alignContentSpaceBetween' | 'alignContentSpaceAround';
+
