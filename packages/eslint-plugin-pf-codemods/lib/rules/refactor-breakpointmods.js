95300fa871e4661b7bf92c62a2ad5e21e4bd8317
evwilkin <evan.wilkinson@gmail.com>
2020-06-01 22:54:53 -0400

    added show rule for ToolbarToggleGroup

--- packages/eslint-plugin-pf-codemods/lib/rules/refactor-breakpointmods.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/refactor-breakpointmods.js
@@ -3,21 +3,50 @@ const { getPackageImports } = require('../helpers');
 // https://github.com/patternfly/patternfly-react/pull/YOURNUMBERHERE
 module.exports = {
   create: function(context) {
+    const componentsToUpdate = [
+      'Flex',
+      'FlexItem',
+      'Page',
+      'ToolbarItem',
+      'ToolbarToggleGroup'
+    ]
     const imports = getPackageImports(context, '@patternfly/react-core')
-      .filter(specifier => specifier.imported.name === 'YOURCOMPONENTNAME');
+      .filter(specifier => componentsToUpdate.includes(specifier.imported.name));
+  
       
     return imports.length == 0 ? {} : {
       JSXOpeningElement(node) {
-        if (imports.map(imp => imp.local.name).includes(node.name.name)) {
-          const attribute = node.attributes.find(node => node.name && node.name.name === 'variant');
-          if (attribute) {
-            context.report({
-              node,
-              message: 'YOUR_MESSAGE_HERE',
-              fix(fixer) {
-                return fixer.replaceText(attribute, '');
-              }
-            });
+        const nodeName = node.name.name;
+        const nodeImport = imports.find(imp => imp.local.name === nodeName); 
+        if (nodeImport) {
+          const importName = nodeImport.imported.name;
+          // Page
+          if (importName === 'Page') {
+            const attribute = node.attributes.find(node => node.name && node.name.name === 'hasNoPadding');
+            if (attribute) {
+              context.report({
+                node,
+                message: `hasNoPadding prop on Page component removed in favor of padding={{ default: 'noPadding' }}`,
+                fix(fixer) {
+                  return fixer.replaceText(attribute, `padding={{ default: 'noPadding' }}`);
+                }
+              });
+            }
+          }
+          // ToolbarToggleGroup
+          if (importName === 'ToolbarToggleGroup') {
+            const attribute = node.attributes.find(attr => attr.name && attr.name.name === 'breakpoint');
+            if (attribute) {
+              const breakpointVal = attribute.value.value;
+              const newAttr = `show={{ ${breakpointVal}: 'show' }}`
+              context.report({
+                node,
+                message: `hasNoPadding prop on Page component removed in favor of padding={{ default: 'noPadding' }}`,
+                fix(fixer) {
+                  return fixer.replaceText(attribute, newAttr);
+                }
+              });
+            }
           }
         }
       }
@@ -77,33 +106,39 @@ const breakpointProps = {
   }
 }
 
-
+const breakpointPropNames = [
+  'visibility',
+  ''
+]
 
 visibility: 'hidden' | 'visible';
-
 widths: 'width_25' | 'width_33' | 'width_50' | 'width_66' | 'width_75' | 'width_100';
-
 padding: 'padding' | 'noPadding';
-
 inset: 'insetNone' | 'insetSm' | 'insetMd' | 'insetLg' | 'insetXl' | 'inset2xl';
-
 alignment: 'alignRight' | 'alignLeft';
-
 spacer?: 'spacerNone' | 'spacerXs' | 'spacerSm' | 'spacerMd' | 'spacerLg' | 'spacerXl' | 'spacer2xl' | 'spacer3xl' | 'spacer4xl';
-
 spaceItems?: 'spaceItemsNone' | 'spaceItemsXs' | 'spaceItemsSm' | 'spaceItemsMd' | 'spaceItemsLg' | 'spaceItemsXl' | 'spaceItems2xl' | 'spaceItems3xl' | 'spaceItems4xl';
-
 show: 'show';
-
 grow: 'grow';
-
 shrink: shrink;
-
 flex: 'flexDefault' | 'flexNone' | 'flex_1' | 'flex_2' | 'flex_3' | 'flex_4';
-
 direction: 'column' | 'columnReverse' | 'row' | 'rowReverse';
-
 alignItems: 'alignItemsFlexStart' | 'alignItemsFlexEnd' | 'alignItemsCenter' | 'alignItemsStretch' | 'alignItemsBaseline';
-
 alignContent: 'alignContentFlexStart' | 'alignContentFlexEnd' | 'alignContentCenter' | 'alignContentStretch' | 'alignContentSpaceBetween' | 'alignContentSpaceAround';
+alignSelf: 'alignSelfFlexStart' | 'alignSelfFlexEnd' | 'alignSelfCenter' | 'alignSelfStretch' | 'alignSelfBaseline';
 
+// Flex
+spacer: ['spacerNone', 'spacerXs', 'spacerSm', 'spacerMd', 'spacerLg', 'spacerXl', 'spacer2xl', 'spacer3xl', 'spacer4xl'],
+spaceItems: ['spaceItemsNone', 'spaceItemsXs', 'spaceItemsSm', 'spaceItemsMd', 'spaceItemsLg', 'spaceItemsXl', 'spaceItems2xl', 'spaceItems3xl', 'spaceItems4xl'],
+grow: ['grow'],
+shrink: ['shrink'],
+flex: ['flexDefault', 'flexNone', 'flex_1', 'flex_2', 'flex_3', 'flex_4'],
+direction: ['column', 'columnReverse', 'row', 'rowReverse'],
+alignItems: ['alignItemsFlexStart', 'alignItemsFlexEnd', 'alignItemsCenter', 'alignItemsStretch', 'alignItemsBaseline'],
+alignContent: ['alignContentFlexStart', 'alignContentFlexEnd', 'alignContentCenter', 'alignContentStretch', 'alignContentSpaceBetween', 'alignContentSpaceAround'],
+alignSelf: ['alignSelfFlexStart', 'alignSelfFlexEnd', 'alignSelfCenter', 'alignSelfStretch', 'alignSelfBaseline'],
+align: ['alignLeft', 'alignRight'],
+justifyContent: ['justifyContentFlexStart', 'justifyContentFlexEnd', 'justifyContentCenter', 'justifyContentSpaceBetween', 'justifyContentSpaceAround', 'justifyContentSpaceEvenly'],
+display: ['inlineFlex'],
+fullWidth: ['fullWidth'],
+flexWrap: ['wrap', 'wrapReverse', 'nowrap']
