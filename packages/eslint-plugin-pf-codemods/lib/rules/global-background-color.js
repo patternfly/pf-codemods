cd57dc467c8e117f51c8e7636234b73351dcba96
Joachim Schuler <jschuler@redhat.com>
2020-06-03 16:07:51 -0400

    background color rule

--- packages/eslint-plugin-pf-codemods/lib/rules/global-background-color.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/global-background-color.js
@@ -5,13 +5,18 @@ module.exports = {
       ImportDeclaration(node) {
         if (node.source.value === '@patternfly/react-tokens') {
           node.specifiers
-            .filter(specifier => ['global_background_color_150', 'global_background_color_300'].includes(specifier.imported.name))
+            .filter(specifier => ['global_BackgroundColor_150', 'global_BackgroundColor_300'].includes(specifier.imported.name))
             .forEach(specifier => {
+              const importedName = specifier.imported.name;
+              const localName = specifier.local.name;
+              const isAliased = importedName !== localName;
+              const aliasText = isAliased ? ` as ${localName}` : ` as ${importedName}`;
+              const newName = `global_BackgroundColor_200${aliasText}`;
               context.report({
                 node,
                 message: `${specifier.imported.name} has been removed. Consider using global_BackgroundColor_200 with its new value #f0f0f0 instead`,
                 fix(fixer) {
-                  return fixer.replaceText(specifier, `global_background_color_200 as ${specifier.imported.name}`);
+                  return fixer.replaceText(specifier, newName);
                 }
               });
             });
--- packages/eslint-plugin-pf-codemods/test/rules/global-background-color.js
+++ packages/eslint-plugin-pf-codemods/test/rules/global-background-color.js
@@ -4,23 +4,23 @@ const rule = require('../../lib/rules/global-background-color');
 ruleTester.run("global-background-color", rule, {
   valid: [
     {
-      code: `import { global_background_color_200 } from '@patternfly/react-tokens';`,
+      code: `import { global_BackgroundColor_200 } from '@patternfly/react-tokens';`,
     }
   ],
   invalid: [
     {
-      code:   `import { global_background_color_300 } from '@patternfly/react-tokens'; const a = global_background_color_300;`,
-      output: `import { global_background_color_200 as global_background_color_300 } from '@patternfly/react-tokens'; const a = global_background_color_300;`,
+      code:   `import { global_BackgroundColor_300 } from '@patternfly/react-tokens'; const a = global_BackgroundColor_300;`,
+      output: `import { global_BackgroundColor_200 as global_BackgroundColor_300 } from '@patternfly/react-tokens'; const a = global_BackgroundColor_300;`,
       errors: [{
-        message: `global_background_color_300 has been removed. Consider using global_BackgroundColor_200 with its new value #f0f0f0 instead`,
+        message: `global_BackgroundColor_300 has been removed. Consider using global_BackgroundColor_200 with its new value #f0f0f0 instead`,
         type: "ImportDeclaration",
       }]
     },
     {
-      code:   `import { global_background_color_150 } from '@patternfly/react-tokens'; const a = global_background_color_150;`,
-      output: `import { global_background_color_200 as global_background_color_150 } from '@patternfly/react-tokens'; const a = global_background_color_150;`,
+      code:   `import { global_BackgroundColor_150 } from '@patternfly/react-tokens'; const a = global_BackgroundColor_150;`,
+      output: `import { global_BackgroundColor_200 as global_BackgroundColor_150 } from '@patternfly/react-tokens'; const a = global_BackgroundColor_150;`,
       errors: [{
-        message: `global_background_color_150 has been removed. Consider using global_BackgroundColor_200 with its new value #f0f0f0 instead`,
+        message: `global_BackgroundColor_150 has been removed. Consider using global_BackgroundColor_200 with its new value #f0f0f0 instead`,
         type: "ImportDeclaration",
       }]
     },
