f852193570328382fb9fcc00f38ae85bf0dda5b9
zallen <47335686+redallen@users.noreply.github.com>
2020-06-08 11:14:49 -0400

    don't rename if new imports exist (#98)

--- packages/eslint-plugin-pf-codemods/lib/rules/card-rename-components.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/card-rename-components.js
@@ -10,19 +10,27 @@ const renames = {
 module.exports = {
   create: function(context) {
     const imports = getPackageImports(context, '@patternfly/react-core')
-      .filter(specifier => Object.keys(renames).includes(specifier.imported.name));
-    
-    return imports.length === 0 ? {} : {
+      .filter(specifier => Object.keys(renames).concat(Object.values(renames)).includes(specifier.imported.name));
+    const cardHeadImport = imports.find(imp => imp.imported.name === 'CardHead');
+    const cardTitleImport = imports.find(imp => imp.imported.name === 'CardTitle');
+    const cardHeaderMainImport = imports.find(imp => imp.imported.name === 'CardHeaderMain');
+
+    // If cardTitleImport || cardHeaderMainImport, assume renames already taken care of
+    return imports.length === 0 || cardTitleImport || cardHeaderMainImport ? {} : {
       ImportDeclaration(node) {
         ensureImports(context, node, '@patternfly/react-core', Object.values(renames));
       },
       JSXIdentifier(node) {
         if (imports.map(imp => imp.local.name).includes(node.name)) {
-          const alreadyFixed = node.parent.parent.openingElement && node.parent.parent.openingElement.attributes
-            .map(attr => attr.name.name)
-            .includes('data-codemods');
+          const alreadyFixed = node.parent // openingElement or closingElement
+            && node.parent.parent // JSXElement
+            && node.parent.parent.openingElement
+            && node.parent.parent.openingElement.attributes
+              .map(attr => attr.name.name)
+              .includes('data-codemods');
+
           if (!alreadyFixed) {
-            const isOpeningCardHead = node.parent.type === 'JSXOpeningElement' && node.name === 'CardHead';
+            const isOpeningCardHead = node.parent.type === 'JSXOpeningElement' && node.name === cardHeadImport.local.name;
             const replacement = `${renames[node.name]}${isOpeningCardHead ? ' data-codemods="true"' : ''}`;
             context.report({
               node,
