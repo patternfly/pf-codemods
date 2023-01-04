9bba680d9ba6eaa3c0f9164a8d7332f046f060e3
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2023-01-04 12:43:21 -0500

    feat(Tooltip): remove deprecated props (#181)
    * feat(Tooltip): remove deprecated props
    
    * update readme output

--- packages/eslint-plugin-pf-codemods/index.js
+++ packages/eslint-plugin-pf-codemods/index.js
@@ -45,6 +45,7 @@ const rules = {
   "chart-remove-allowZoom": require('./lib/rules/v4/chartVoronoiContainer-remove-allowTooltip'),
   "react-icons-remove-icon": require('./lib/rules/v4/react-icons-remove-icon'),
   "toolbar-remove-visiblity": require('./lib/rules/v5/toolbar-remove-visiblity'),
+  "tooltip-remove-props": require('./lib/rules/v5/tooltip-remove-props'),
   "divider-remove-isVertical": require('./lib/rules/v5/divider-remove-isVertical'),
 };
 
