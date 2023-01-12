aff300ffb1abe1d14f49267678b6fe6c6dbb66fe
Katie McFaul <kmcfaul@redhat.com>
2023-01-12 16:19:04 -0500

    only true isSVG should be removed

--- packages/eslint-plugin-pf-codemods/lib/rules/v5/spinner-svg-default.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/v5/spinner-svg-default.js
@@ -1,12 +1,28 @@
-const { renameProp } = require('../../helpers');
+const { getPackageImports } = require('../../helpers');
 
 // https://github.com/patternfly/patternfly-react/pull/8183
 module.exports = {
   meta: { fixable: 'code' },
-  create: renameProp(
-    'Spinner',
-    { isSVG: '' },
-    () => `Spinner isSVG prop default value has changed from false to true.`
-  )
+  create: function(context) {
+    const spinnerImports = getPackageImports(context, '@patternfly/react-core')
+      .filter(specifier => specifier.imported.name == 'Spinner');
+      
+    return spinnerImports.length === 0 ? {} : {
+      JSXOpeningElement(node) {
+        if (spinnerImports.map(imp => imp.local.name).includes(node.name.name)) {
+          const svgAttribute = node.attributes.find(n => n.name && n.name.name === 'isSVG');
+          if (svgAttribute && svgAttribute?.value?.expression?.value !== false) {
+            context.report({
+              node,
+              message: `Spinner isSVG prop default value has changed from false to true.`,
+              fix(fixer) {
+                return fixer.replaceText(svgAttribute, ``);
+              }
+            });
+          }
+        }
+      }
+    };
+  }
 };
 
--- packages/eslint-plugin-pf-codemods/test/rules/v5/spinner-svg-default.js
+++ packages/eslint-plugin-pf-codemods/test/rules/v5/spinner-svg-default.js
@@ -7,6 +7,9 @@ ruleTester.run("spinner-svg-default", rule, {
       code: `import { Spinner } from '@patternfly/react-core'; <Spinner />`,
     },
     {
+      code: `import { Spinner } from '@patternfly/react-core'; <Spinner isSVG={false} />`,
+    },
+    {
       // No @patternfly/react-core import
       code: `<Spinner isSVG />`,
     }
@@ -14,6 +17,14 @@ ruleTester.run("spinner-svg-default", rule, {
   invalid: [
     {
       code:   `import { Spinner } from '@patternfly/react-core'; <Spinner isSVG />`,
+      output: `import { Spinner } from '@patternfly/react-core'; <Spinner  />`,
+      errors: [{
+        message: `Spinner isSVG prop default value has changed from false to true.`,
+        type: "JSXOpeningElement",
+      }]
+    },
+    {
+      code:   `import { Spinner } from '@patternfly/react-core'; <Spinner isSVG={true} />`,
       output: `import { Spinner } from '@patternfly/react-core'; <Spinner  />`,
       errors: [{
         message: `Spinner isSVG prop default value has changed from false to true.`,
