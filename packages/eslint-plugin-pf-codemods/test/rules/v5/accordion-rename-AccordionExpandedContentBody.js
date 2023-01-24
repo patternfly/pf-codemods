d6c7cc03a091f3b6281cc62afb570404229e2c7f
Jenny <32821331+jenny-s51@users.noreply.github.com>
2023-01-24 08:27:16 -0500

    feat(AccordionExpandableContent): rename expanded refs (#217)
    * rename expanded refs
    
    add import to test.tsx
    
    fix case
    
    fix path
    
    * update error msgs
    
    * fix output
    
    * README

---
+++ packages/eslint-plugin-pf-codemods/test/rules/v5/accordion-rename-AccordionExpandedContentBody.js
@@ -0,0 +1,36 @@
+const ruleTester = require('../../ruletester');
+const rule = require('../../../lib/rules/v5/accordion-rename-AccordionExpandedContentBody');
+
+ruleTester.run("accordion-rename-AccordionExpandedContentBody", rule, {
+  valid: [
+    {
+      code: `import { AccordionExpandableContentBody } from '@patternfly/react-core';
+      <AccordionExpandableContentBody>Body</AccordionExpandableContentBody>
+      `,
+    },
+    {
+      // No @patternfly/react-core import
+      code: `<AccordionExpandedContentBody>Body</AccordionExpandedContentBody>`,
+    },
+  ],
+  invalid: [
+    {
+      code: `import { AccordionExpandedContentBody } from '@patternfly/react-core'; <AccordionExpandedContentBody>Body</AccordionExpandedContentBody>`,
+      output: `import { AccordionExpandedContentBody, AccordionExpandableContentBody } from '@patternfly/react-core'; <AccordionExpandableContentBody>Body</AccordionExpandableContentBody>`,
+      errors: [
+        {
+          message: `add missing imports AccordionExpandableContentBody from @patternfly/react-core`,
+          type: "ImportDeclaration",
+        },
+        {
+          message: "AccordionExpandedContentBody has been replaced with AccordionExpandableContentBody",
+          type: "JSXIdentifier",
+        },
+        {
+          message: "AccordionExpandedContentBody has been replaced with AccordionExpandableContentBody",
+          type: "JSXIdentifier",
+        },
+      ],
+    },
+  ],
+});
