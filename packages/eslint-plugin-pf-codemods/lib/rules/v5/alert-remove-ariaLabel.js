fc04a01376abb2a905026ff09b289bb4ae6d937b
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2023-02-10 16:51:48 -0500

    chore(Alert): removed aria-label prop (#251)
    * chore(Alert): removed aria-label prop
    
    * Fixed typo

---
+++ packages/eslint-plugin-pf-codemods/lib/rules/v5/alert-remove-ariaLabel.js
@@ -0,0 +1,12 @@
+const { renameProp } = require("../../helpers");
+
+// https://github.com/patternfly/patternfly-react/pull/8649
+module.exports = {
+  meta: { fixable: "code" },
+  create: renameProp(
+    "Alert",
+    { "aria-label": "" },
+    (node, attribute) =>
+      `${attribute.name.name} prop for ${node.name.name} has been removed and should not be used as it is not well supported on div elements without a role.`
+  ),
+};
