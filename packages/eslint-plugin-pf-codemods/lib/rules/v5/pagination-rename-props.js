5b98a39b9591526a5574b8831f898d19e056aa19
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2023-03-02 12:51:27 -0500

    fix(Pagination): updated codemod for prop renaming (#302)
    * fix(Pagination): updated codemod for prop renaming
    
    * Added test for direct prop and titles subprop together

--- packages/eslint-plugin-pf-codemods/lib/rules/v5/pagination-rename-props.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/v5/pagination-rename-props.js
@@ -1,21 +1,92 @@
-const { renameProps } = require("../../helpers");
+const { getPackageImports } = require("../../helpers");
 
-const renames = {
-  Pagination: {
-    currPage: "currPageAriaLabel",
-    paginationTitle: "paginationAriaLabel",
-    toFirstPage: "toFirstPageAriaLabel",
-    toLastPage: "toLastPageAriaLabel",
-    toNextPage: "toNextPageAriaLabel",
-    toPreviousPage: "toPreviousPageAriaLabel",
-    optionsToggle: "optionsToggleAriaLabel",
-    defaultToFullPage: "isLastFullPageShown",
-    perPageComponenet: "",
-  },
-};
-
-// https://github.com/patternfly/patternfly-react/pull/8319
 module.exports = {
   meta: { fixable: "code" },
-  create: renameProps(renames),
+  create: function (context) {
+    const paginationImport = getPackageImports(
+      context,
+      "@patternfly/react-core"
+    ).find((specifier) => specifier.imported.name === "Pagination");
+
+    return !paginationImport
+      ? {}
+      : {
+          JSXOpeningElement(node) {
+            if (node.name.name === paginationImport.local.name) {
+              const updatedPropNames = {
+                perPageComponent: "",
+                defaultToFullPage: "isLastFullPageShown",
+              };
+
+              const existingProps = node.attributes.filter((attribute) =>
+                Object.keys(updatedPropNames).includes(attribute.name.name)
+              );
+
+              if (existingProps.length) {
+                existingProps.forEach((existingProp) => {
+                  const newPropName = updatedPropNames[existingProp.name.name];
+                  context.report({
+                    node,
+                    message: `The "${
+                      existingProp.name.name
+                    }" prop for Pagination has been ${
+                      newPropName ? `renamed to "${newPropName}"` : "removed"
+                    }.`,
+                    fix(fixer) {
+                      return [
+                        fixer.replaceText(
+                          existingProp,
+                          updatedPropNames[existingProp.name.name]
+                        ),
+                      ];
+                    },
+                  });
+                });
+              }
+
+              const titlesProp = node.attributes.find(
+                (attribute) => attribute.name.name === "titles"
+              );
+
+              if (titlesProp?.value?.expression?.type === "ObjectExpression") {
+                const updatedTitlesPropNames = {
+                  currPage: "currPageAriaLabel",
+                  paginationTitle: "paginationAriaLabel",
+                  toFirstPage: "toFirstPageAriaLabel",
+                  toLastPage: "toLastPageAriaLabel",
+                  toNextPage: "toNextPageAriaLabel",
+                  toPreviousPage: "toPreviousPageAriaLabel",
+                  optionsToggle: "optionsToggleAriaLabel",
+                };
+
+                const existingTitlesProps =
+                  titlesProp.value.expression.properties.filter((property) =>
+                    Object.keys(updatedTitlesPropNames).includes(
+                      property.key.name
+                    )
+                  );
+
+                if (existingTitlesProps.length) {
+                  existingTitlesProps.forEach((existingTitlesProp) => {
+                    const newTitlesPropName =
+                      updatedTitlesPropNames[existingTitlesProp.key.name];
+                    context.report({
+                      node,
+                      message: `The "${existingTitlesProp.key.name}" sub-prop for Pagination's "titles" prop has been renamed to "${newTitlesPropName}".`,
+                      fix(fixer) {
+                        return [
+                          fixer.replaceTextRange(
+                            existingTitlesProp.key.range,
+                            newTitlesPropName
+                          ),
+                        ];
+                      },
+                    });
+                  });
+                }
+              }
+            }
+          },
+        };
+  },
 };
--- packages/eslint-plugin-pf-codemods/test/rules/v5/pagination-rename-props.js
+++ packages/eslint-plugin-pf-codemods/test/rules/v5/pagination-rename-props.js
@@ -1,106 +1,87 @@
 const ruleTester = require("../../ruletester");
 const rule = require("../../../lib/rules/v5/pagination-rename-props");
 
+const updatedPropNames = {
+  perPageComponent: "",
+  defaultToFullPage: "isLastPageShown",
+};
+const updatedTitlesPropNames = {
+  currPage: "currPageAriaLabel",
+  paginationTitle: "paginationAriaLabel",
+  toFirstPage: "toFirstPageAriaLabel",
+  toLastPage: "toLastPageAriaLabel",
+  toNextPage: "toNextPageAriaLabel",
+  toPreviousPage: "toPreviousPageAriaLabel",
+  optionsToggle: "optionsToggleAriaLabel",
+};
+
 ruleTester.run("pagination-rename-props", rule, {
   valid: [
     {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination />`,
+      code: `import { Pagination } from '@patternfly/react-core'; <Pagination titles={{items: "test"}} />`,
     },
     {
       // No @patternfly/react-core import
-      code: `<Pagination currPage paginationTitle optionsToggle paginationTitle toFirstPage toLastPage toNextPage toPreviousPage defaultToFullPage perPageComponenet />`,
+      code: `<Pagination defaultToFullPage />`,
     },
+    {
+      // No @patternfly/react-core import
+      code: `<Pagination perPageComponent="div" />`,
+    },
+    // No @patternfly/react-core import
+    ...Object.keys(updatedTitlesPropNames).map((titlesPropName) => ({
+      code: `<Pagination titles={{${titlesPropName}: "test"}} />`,
+    })),
+    ,
   ],
   invalid: [
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination currPage="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination currPageAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `currPage prop for Pagination has been renamed to currPageAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination paginationTitle="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination paginationAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `paginationTitle prop for Pagination has been renamed to paginationAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination toFirstPage="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination toFirstPageAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `toFirstPage prop for Pagination has been renamed to toFirstPageAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination toLastPage="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination toLastPageAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `toLastPage prop for Pagination has been renamed to toLastPageAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination toNextPage="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination toNextPageAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `toNextPage prop for Pagination has been renamed to toNextPageAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination toPreviousPage="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination toPreviousPageAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `toPreviousPage prop for Pagination has been renamed to toPreviousPageAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
-    {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination optionsToggle="Text" />`,
-      output: `import { Pagination } from '@patternfly/react-core'; <Pagination optionsToggleAriaLabel="Text" />`,
-      errors: [
-        {
-          message: `optionsToggle prop for Pagination has been renamed to optionsToggleAriaLabel`,
-          type: "JSXOpeningElement",
-        },
-      ],
-    },
     {
       code: `import { Pagination } from '@patternfly/react-core'; <Pagination defaultToFullPage />`,
       output: `import { Pagination } from '@patternfly/react-core'; <Pagination isLastFullPageShown />`,
       errors: [
         {
-          message: `defaultToFullPage prop for Pagination has been renamed to isLastFullPageShown`,
+          message: `The "defaultToFullPage" prop for Pagination has been renamed to "isLastFullPageShown".`,
           type: "JSXOpeningElement",
         },
       ],
     },
     {
-      code: `import { Pagination } from '@patternfly/react-core'; <Pagination perPageComponenet="div" />`,
+      code: `import { Pagination } from '@patternfly/react-core'; <Pagination perPageComponent="div" />`,
       output: `import { Pagination } from '@patternfly/react-core'; <Pagination  />`,
       errors: [
         {
-          message: `perPageComponenet prop for Pagination has been removed`,
+          message: `The "perPageComponent" prop for Pagination has been removed.`,
           type: "JSXOpeningElement",
         },
       ],
     },
+    ...Object.keys(updatedTitlesPropNames).map((titlesPropName) => ({
+      code: `import { Pagination } from '@patternfly/react-core'; <Pagination titles={{${titlesPropName}: "test"}} />`,
+      output: `import { Pagination } from '@patternfly/react-core'; <Pagination titles={{${updatedTitlesPropNames[titlesPropName]}: "test"}} />`,
+      errors: [
+        {
+          message: `The "${titlesPropName}" sub-prop for Pagination's "titles" prop has been renamed to "${updatedTitlesPropNames[titlesPropName]}".`,
+          type: "JSXOpeningElement",
+        },
+      ],
+    })),
+    ...Object.keys(updatedTitlesPropNames).map((titlesPropName) => ({
+      code: `import { Pagination } from '@patternfly/react-core'; <Pagination defaultToFullPage perPageComponent="div" titles={{${titlesPropName}: "test"}} />`,
+      output: `import { Pagination } from '@patternfly/react-core'; <Pagination isLastFullPageShown  titles={{${updatedTitlesPropNames[titlesPropName]}: "test"}} />`,
+      errors: [
+        {
+          message: `The "defaultToFullPage" prop for Pagination has been renamed to "isLastFullPageShown".`,
+          type: "JSXOpeningElement",
+        },
+        {
+          message: `The "perPageComponent" prop for Pagination has been removed.`,
+          type: "JSXOpeningElement",
+        },
+        {
+          message: `The "${titlesPropName}" sub-prop for Pagination's "titles" prop has been renamed to "${updatedTitlesPropNames[titlesPropName]}".`,
+          type: "JSXOpeningElement",
+        },
+      ],
+    })),
   ],
 });
