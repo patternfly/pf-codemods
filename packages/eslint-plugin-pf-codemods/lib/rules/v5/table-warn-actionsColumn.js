29045a8e5fccca6218b61fd055015916a9ca4236
Katie McFaul <kmcfaul@redhat.com>
2023-03-24 17:45:26 -0400

    chore(Table): refine ActionsColumn warning

--- packages/eslint-plugin-pf-codemods/lib/rules/v5/table-warn-actionsColumn.js
+++ packages/eslint-plugin-pf-codemods/lib/rules/v5/table-warn-actionsColumn.js
@@ -1,18 +1,33 @@
+const { getPackageImports } = require('../../helpers');
+
 // https://github.com/patternfly/patternfly-react/pull/8629
 module.exports = {
   create: function (context) {
     return {
-      ImportDeclaration(node) {
-        const TableImport = node.specifiers.find(
+      Program(node) {
+        const coreImports = getPackageImports(context, '@patternfly/react-core');
+        const tableImports = getPackageImports(context, '@patternfly/react-table');
+        const imports = [...coreImports, ...tableImports];
+
+        const tableImport = imports.find(
           (specifier) =>
-            node.source.value === "@patternfly/react-table" &&
             (specifier.imported.name === "Table" || specifier.imported.name === "TableComposable")
         );
 
-        if (TableImport) {
+        const dropdownToggleImport = imports.find(
+          (specifier) =>
+            (specifier.imported.name === "DropdownToggle")
+        );
+
+        const actionsColumnImport = imports.find(
+          (specifier) =>
+            (specifier.imported.name === "ActionsColumn")
+        );
+
+        if (tableImport && (dropdownToggleImport || actionsColumnImport)) {
           context.report({
             node,
-            message: "The ActionsColumn now uses the Next version of Dropdown. The action toggle should now pass a MenuToggle rather than a DropdownToggle, and direction and position properties are now passed under the new popperProps property.",
+            message: "The ActionsColumn now uses the Next version of Dropdown. The action toggle should now pass a MenuToggle rather than a DropdownToggle, and direction and position properties are now passed under the ActionsColumn new popperProps property.",
           });
         }
       },
--- packages/eslint-plugin-pf-codemods/test/rules/v5/table-warn-actionsColumn.js
+++ packages/eslint-plugin-pf-codemods/test/rules/v5/table-warn-actionsColumn.js
@@ -4,6 +4,12 @@ const rule = require('../../../lib/rules/v5/table-warn-actionsColumn');
 ruleTester.run("table-warn-actionsColumn", rule, {
   valid: [
     {
+      code: `import { Table } from '@patternfly/react-table'; <Table />`,
+    },
+    {
+      code: `import { TableComposable } from '@patternfly/react-table'; <TableComposable />`,
+    },
+    {
       code: `<Table />`,
     },
     {
@@ -12,19 +18,19 @@ ruleTester.run("table-warn-actionsColumn", rule, {
   ],
   invalid: [
     {
-      code:   `import { Table } from '@patternfly/react-table';`,
-      output: `import { Table } from '@patternfly/react-table';`,
+      code:   `import { Table } from '@patternfly/react-table'; import { DropdownToggle } from '@patternfly/react-core';`,
+      output: `import { Table } from '@patternfly/react-table'; import { DropdownToggle } from '@patternfly/react-core';`,
       errors: [{
-        message: `The ActionsColumn now uses the Next version of Dropdown. The action toggle should now pass a MenuToggle rather than a DropdownToggle, and direction and position properties are now passed under the new popperProps property.`,
-        type: "ImportDeclaration",
+        message: `The ActionsColumn now uses the Next version of Dropdown. The action toggle should now pass a MenuToggle rather than a DropdownToggle, and direction and position properties are now passed under the ActionsColumn new popperProps property.`,
+        type: "Program",
       }]
     },
     {
-      code:   `import { TableComposable } from '@patternfly/react-table';`,
-      output: `import { TableComposable } from '@patternfly/react-table';`,
+      code:   `import { TableComposable, ActionsColumn } from '@patternfly/react-table';`,
+      output: `import { TableComposable, ActionsColumn } from '@patternfly/react-table';`,
       errors: [{
-        message: `The ActionsColumn now uses the Next version of Dropdown. The action toggle should now pass a MenuToggle rather than a DropdownToggle, and direction and position properties are now passed under the new popperProps property.`,
-        type: "ImportDeclaration",
+        message: `The ActionsColumn now uses the Next version of Dropdown. The action toggle should now pass a MenuToggle rather than a DropdownToggle, and direction and position properties are now passed under the ActionsColumn new popperProps property.`,
+        type: "Program",
       }]
     },
   ]
