74d14e5f60f3c9e60c5aae40e9c3ba9a181e753e
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2023-03-29 15:20:12 -0400

    chore(Menu): rename MenuInput (#349)
    * chore(Menu): rename MenuInput
    
    * rename MenuInput

---
+++ packages/eslint-plugin-pf-codemods/lib/rules/v5/menu-search-rename.js
@@ -0,0 +1,54 @@
+const { getPackageImports, ensureImports } = require('../../helpers');
+
+// https://github.com/patternfly/patternfly-react/pull/8820
+module.exports = {
+  meta: { fixable: 'code' },
+  create: function(context) { 
+    const imports = getPackageImports(context, '@patternfly/react-core');
+    const menuInputImport = imports.find(imp => imp.imported.name === 'MenuInput');
+
+    return imports.length === 0 || !menuInputImport ? {} : { 
+      ImportDeclaration(node) {
+        if(menuInputImport) {
+          context.report({
+            node,
+            message: 'add missing imports MenuSearch, MenuSearchInput from @patternfly/react-core',
+            fix(fixer) {
+              const fixes = [];
+              
+              fixes.push(
+                fixer.replaceText(menuInputImport, 'MenuSearch'),
+                fixer.insertTextAfter(node.specifiers[node.specifiers.length - 1], `, MenuSearchInput`)
+              );
+              
+              return fixes;
+            }
+          })
+        }
+      },
+      JSXOpeningElement(node) {
+        if (node.name.name === menuInputImport.local.name) {
+          context.report({
+            node,
+            message: "MenuInput has been renamed to MenuSearchInput and MenuSearch has been added as a wrapper.",
+            fix(fixer) {
+              const fixes = [];
+
+              fixes.push(
+                fixer.insertTextBefore(node, "<MenuSearch>"),
+                fixer.replaceText(node.name, "MenuSearchInput"),
+                fixer.insertTextAfter(node.parent, "</MenuSearch>")
+              );
+
+              if(node?.parent?.closingElement) {
+                fixes.push(fixer.replaceText(node.parent.closingElement.name, "MenuSearchInput"));
+              }
+              
+              return fixes;
+            }
+          });
+        }
+      }
+    };
+  }
+};
