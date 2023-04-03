aab4642e7b8405e0c675a4fda921b211a6c47672
Austin Sullivan <ausulliv@redhat.com>
2023-04-03 13:26:47 -0400

    chore(generators): Fix issue where test alphabetization moved children

--- generators/update-test-single.js
+++ generators/update-test-single.js
@@ -31,7 +31,9 @@ function baseTestSingle(componentName, componentUsage) {
     trimmedImportedComponents.sort();
   }
 
-  const formattedUpdatedImports = trimmedImportedComponents.filter(component => component.match(/\w/)).join(",\n  ");
+  const formattedUpdatedImports = trimmedImportedComponents
+    .filter((component) => component.match(/\w/))
+    .join(",\n  ");
 
   const newCoreImport = [
     "\n  ",
@@ -55,9 +57,28 @@ function baseTestSingle(componentName, componentUsage) {
   const splitByFragment = contentAfterAddingImport.split("\n<>");
   const fragmentSection = splitByFragment[1];
   const components = fragmentSection.split(/\/>;?\n/);
-  components.push(componentUsage);
-  components.sort().push(components.shift());
-  const newFragmentSection = components.join("/>\n");
+
+  // This identifies child components based on indent level and concatenates those children with their parent so that
+  // children aren't alphabetized independently
+  const componentsConcatChildren = components.reduce((acc, component, i) => {
+    const componentIndentLevel =
+      component.split("<")[0].match(/\s/g)?.length / 2;
+    const previousComponent = acc[i - 1];
+
+    if (componentIndentLevel < 2 || !previousComponent) {
+      acc.push(component);
+    } else if (previousComponent[previousComponent.length - 1] === " ") {
+      acc[i - 1] += "/>\n" + component;
+    } else {
+      acc[i - 1] += component;
+    }
+
+    return acc;
+  }, []);
+
+  componentsConcatChildren.push(componentUsage);
+  componentsConcatChildren.sort().push(componentsConcatChildren.shift());
+  const newFragmentSection = componentsConcatChildren.join("/>\n");
   const newContent = [splitByFragment[0], newFragmentSection].join("\n<>");
 
   fs.writeFileSync(testPath, newContent);
