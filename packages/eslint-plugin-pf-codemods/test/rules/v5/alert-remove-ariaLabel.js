6151a2c667012880cc3685a0fbd698741575bd6a
gitdallas <dallas.nicol@gmail.com>
2023-04-18 13:34:21 -0500

    adding some tests

--- packages/eslint-plugin-pf-codemods/test/rules/v5/alert-remove-ariaLabel.js
+++ packages/eslint-plugin-pf-codemods/test/rules/v5/alert-remove-ariaLabel.js
@@ -32,5 +32,25 @@ ruleTester.run("alert-remove-ariaLabel", rule, {
         },
       ],
     },
+    {
+      code: `import { Alert } from '@patternfly/react-core/dist/esm/components/Alert/index.js'; <Alert aria-label="tester" />`,
+      output: `import { Alert } from '@patternfly/react-core/dist/esm/components/Alert/index.js'; <Alert  />`,
+      errors: [
+        {
+          message: `aria-label prop for Alert has been removed and should not be used as it is not well supported on div elements without a role.`,
+          type: "JSXOpeningElement",
+        },
+      ],
+    },
+    {
+      code: `import { Alert as Al } from '@patternfly/react-core/dist/esm/components/Alert/index.js'; <Al aria-label="tester" />`,
+      output: `import { Alert as Al } from '@patternfly/react-core/dist/esm/components/Alert/index.js'; <Al  />`,
+      errors: [
+        {
+          message: `aria-label prop for Al has been removed and should not be used as it is not well supported on div elements without a role.`,
+          type: "JSXOpeningElement",
+        },
+      ],
+    },
   ],
 });
