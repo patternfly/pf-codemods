56f78568fb8df518b88bf998dcce9a923668e41e
Jenny <32821331+jenny-s51@users.noreply.github.com>
2023-05-15 10:53:59 -0400

    feat(Table): rename TableComposable to Table (#388)
    * rename table composable
    
    add examples
    
    PR feedback from Eric
    
    fix var name
    
    update table composable rule
    
    fix conflicts
    
    specify table rule order
    
    add logic from Eric to fix multiple codemod runs
    
    remove rule from cleanup rules
    
    dont rename aliased imports
    
    const fix
    
    PR suggestion from Dallas
    
    update to deep equal
    
    fix undefined error
    
    PR feedback from Dallas
    
    PR feedback from Dallas
    
    add missing In
    
    * remove unnecessary code block
    
    * Update packages/eslint-plugin-pf-codemods/lib/rules/v5/table-rename-TableComposable.js
    
    Co-authored-by: Dallas <dallas.nicol@gmail.com>
    
    ---------
    
    Co-authored-by: Dallas <dallas.nicol@gmail.com>

--- test/test.tsx
+++ test/test.tsx
@@ -111,6 +111,9 @@ import {
   TableHeader,
   TableProps,
 } from "@patternfly/react-table";
+import { TableComposable } from "@patternfly/react-table";
+import { TableComposable as PFTable } from "@patternfly/react-table";
+
 
 //following type of import was causing errors for rules that checked specifiers before import package
 import foo from "Bar";
@@ -230,6 +233,7 @@ const alertVariantOption = AlertVariant.default;
     }}
     toggleTemplate={({ first, second }: ToggleTemplateProps) => <></>}
   />
+  <PFTable>Body</PFTable>
   <Popover reference alertSeverityVariant='default' />
   <Popover
     shouldClose={(foo, event) => handler(foo, event)}
@@ -245,6 +249,7 @@ const alertVariantOption = AlertVariant.default;
   <Spinner isSVG />
   <Table />
   <TableBody />
+  <TableComposable />
   <TableHeader />
   <Td select={tdSelectTypeObj} actions={{ disable: false }} />
   <TextInput onChange={(foo, event) => handler(foo, event)} />
