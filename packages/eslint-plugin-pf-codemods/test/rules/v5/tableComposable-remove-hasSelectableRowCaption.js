983880f0e3dda0a8f62385add774155666bb8d60
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2023-06-02 15:25:14 -0400

    fix(TableComposable): removed hasSelectableRowCaption after rename (#506)

--- packages/eslint-plugin-pf-codemods/test/rules/v5/tableComposable-remove-hasSelectableRowCaption.js
+++ packages/eslint-plugin-pf-codemods/test/rules/v5/tableComposable-remove-hasSelectableRowCaption.js
@@ -1,5 +1,5 @@
-const ruleTester = require('../../ruletester');
-const rule = require('../../../lib/rules/v5/tableComposable-remove-hasSelectableRowCaption');
+const ruleTester = require("../../ruletester");
+const rule = require("../../../lib/rules/v5/tableComposable-remove-hasSelectableRowCaption");
 
 ruleTester.run("tableComposable-remove-hasSelectableRowCaption", rule, {
   valid: [
@@ -12,24 +12,39 @@ ruleTester.run("tableComposable-remove-hasSelectableRowCaption", rule, {
     {
       // No @patternfly/react-core import
       code: `<TableComposable hasSelectableRowCaption />`,
-    }
+    },
   ],
   invalid: [
     {
-      code:   `import { TableComposable } from '@patternfly/react-table'; <TableComposable hasSelectableRowCaption />`,
+      code: `import { TableComposable } from '@patternfly/react-table'; <TableComposable hasSelectableRowCaption />`,
       output: `import { TableComposable } from '@patternfly/react-table'; <TableComposable  />`,
-      errors: [{
-        message: `hasSelectableRowCaption prop for TableComposable has been removed`,
-        type: "JSXOpeningElement",
-      }]
+      errors: [
+        {
+          message: `hasSelectableRowCaption prop for TableComposable has been removed`,
+          type: "JSXOpeningElement",
+        },
+      ],
     },
     {
-      code:   `import { TableComposable } from '@patternfly/react-table/dist/esm/components/TableComposable/index.js'; <TableComposable hasSelectableRowCaption />`,
+      code: `import { TableComposable } from '@patternfly/react-table/dist/esm/components/TableComposable/index.js'; <TableComposable hasSelectableRowCaption />`,
       output: `import { TableComposable } from '@patternfly/react-table/dist/esm/components/TableComposable/index.js'; <TableComposable  />`,
-      errors: [{
-        message: `hasSelectableRowCaption prop for TableComposable has been removed`,
-        type: "JSXOpeningElement",
-      }]
+      errors: [
+        {
+          message: `hasSelectableRowCaption prop for TableComposable has been removed`,
+          type: "JSXOpeningElement",
+        },
+      ],
     },
-  ]
+    // TableComposable has been renamed from table-rename-tableComposable
+    {
+      code: `import { Table /* data-codemods */ } from '@patternfly/react-table'; <Table hasSelectableRowCaption />`,
+      output: `import { Table /* data-codemods */ } from '@patternfly/react-table'; <Table  />`,
+      errors: [
+        {
+          message: `hasSelectableRowCaption prop for Table has been removed`,
+          type: "JSXOpeningElement",
+        },
+      ],
+    },
+  ],
 });
