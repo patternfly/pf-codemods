6311e00955c0353bc2c2bf811f14d5e7f7a861b5
adamviktora <viktora.adam@gmail.com>
2024-02-21 17:52:20 +0100

    feat(Checkbox/Radio): replace isLabelBeforeButton with labelPosition="start"

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/checkboxRadioReplaceIsLabelBeforeButton/checkboxRadioReplaceIsLabelBeforeButtonInput.tsx
@@ -0,0 +1,8 @@
+import { Checkbox, Radio } from "@patternfly/react-core";
+
+export const CheckboxReplaceIsLabelBeforeButtonInput = () => (
+  <Checkbox isLabelBeforeButton />
+);
+export const RadioReplaceIsLabelBeforeButtonInput = () => (
+  <Radio isLabelBeforeButton />
+);
---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/checkboxRadioReplaceIsLabelBeforeButton/checkboxRadioReplaceIsLabelBeforeButtonOutput.tsx
@@ -0,0 +1,8 @@
+import { Checkbox, Radio } from "@patternfly/react-core";
+
+export const CheckboxReplaceIsLabelBeforeButtonInput = () => (
+  <Checkbox labelPosition="start" />
+);
+export const RadioReplaceIsLabelBeforeButtonInput = () => (
+  <Radio labelPosition="start" />
+);
