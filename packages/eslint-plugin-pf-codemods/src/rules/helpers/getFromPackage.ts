cda443e1ec51fe22c6ad30410ef6687e6634711a
Austin Sullivan <ausulliv@redhat.com>
2024-02-21 09:11:17 -0500

    chore(devex): Add types for getFromPackage and pfPackageMatcher helpers (#577)
    * chore(devex): Add types for getFromPackage and pfPackageMatcher helpers
    
    * chore(helpers): add helper imports to helper.js file
    
    * chore(helpers): Refactor to use .getSourceCode() instead of .sourceCode

---
+++ packages/eslint-plugin-pf-codemods/src/rules/helpers/getFromPackage.ts
@@ -0,0 +1,72 @@
+import { Rule } from "eslint";
+import {
+  ImportDeclaration,
+  ModuleDeclaration,
+  Statement,
+  Directive,
+  ExportNamedDeclaration,
+  ImportSpecifier,
+  ExportSpecifier,
+} from "estree-jsx";
+import { pfPackageMatches } from "./pfPackageMatches";
+
+type Declarations = ImportDeclaration | ExportNamedDeclaration;
+type Specifiers = ImportSpecifier | ExportSpecifier;
+
+function filterByPackageName(nodes: Declarations[], packageName: string) {
+  return nodes.filter((node) => {
+    const nodeValue = node?.source?.value;
+
+    if (typeof nodeValue !== "string") {
+      return false;
+    }
+    if (packageName.startsWith("@patternfly")) {
+      return pfPackageMatches(packageName, nodeValue);
+    }
+    return nodeValue === packageName;
+  });
+}
+
+function getSpecifiers<
+  NodeType extends Declarations,
+  SpecifierType extends Specifiers
+>(
+  astBody: (ModuleDeclaration | Statement | Directive)[],
+  nodeType: "ImportDeclaration" | "ExportNamedDeclaration",
+  packageName: string
+): SpecifierType[] {
+  const nodesOfSpecifiedType = astBody.filter(
+    (node) => node?.type === nodeType
+  ) as NodeType[];
+  const pfNodes = filterByPackageName(nodesOfSpecifiedType, packageName);
+  const specifiers = pfNodes.map((node) => node.specifiers);
+
+  const specifierType =
+    nodeType === "ImportDeclaration" ? "ImportSpecifier" : "ExportSpecifier";
+
+  const filteredSpecifiersByType = specifiers.filter((specifierArray) =>
+    specifierArray.every((specifier) => specifier.type === specifierType)
+  ) as unknown as SpecifierType[];
+
+  return filteredSpecifiersByType.reduce(
+    (acc, val) => acc.concat(val),
+    [] as SpecifierType[]
+  );
+}
+
+export function getFromPackage(context: Rule.RuleContext, packageName: string) {
+  const astBody = context.getSourceCode().ast.body;
+
+  const imports = getSpecifiers<ImportDeclaration, ImportSpecifier>(
+    astBody,
+    "ImportDeclaration",
+    packageName
+  );
+  const exports = getSpecifiers<ExportNamedDeclaration, ExportSpecifier>(
+    astBody,
+    "ExportNamedDeclaration",
+    packageName
+  );
+
+  return { imports, exports };
+}
