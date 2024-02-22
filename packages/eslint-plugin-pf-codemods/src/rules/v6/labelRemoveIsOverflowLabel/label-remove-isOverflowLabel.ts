91b73b553af272d49f7fbcb31768f46e1b54b305
adamviktora <viktora.adam@gmail.com>
2024-02-22 10:11:44 +0100

    update types

--- packages/eslint-plugin-pf-codemods/src/rules/v6/labelRemoveIsOverflowLabel/label-remove-isOverflowLabel.ts
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/labelRemoveIsOverflowLabel/label-remove-isOverflowLabel.ts
@@ -1,15 +1,11 @@
 import { getFromPackage } from "../../helpers";
+import { Rule } from "eslint";
+import { JSXOpeningElement } from "estree-jsx";
 
 // https://github.com/patternfly/patternfly-react/pull/10037
 module.exports = {
   meta: { fixable: "code" },
-  create: function (context: {
-    report: (arg0: {
-      node: any;
-      message: string;
-      fix(fixer: any): any;
-    }) => void;
-  }) {
+  create: function (context: Rule.RuleContext) {
     const { imports } = getFromPackage(context, "@patternfly/react-core");
 
     const labelImport = imports.find(
@@ -20,16 +16,21 @@ module.exports = {
     return !labelImport
       ? {}
       : {
-          JSXOpeningElement(node: { name: { name: any }; attributes: any[] }) {
-            if (labelImport.local.name === node.name.name) {
+          JSXOpeningElement(node: JSXOpeningElement) {
+            if (
+              node.name.type === "JSXIdentifier" &&
+              labelImport.local.name === node.name.name
+            ) {
               const isOverflowLabelAttribute = node.attributes.find(
-                (attr: { name: { name: string } }) =>
+                (attr) =>
+                  attr.type === "JSXAttribute" &&
                   attr.name?.name === "isOverflowLabel"
               );
 
               if (isOverflowLabelAttribute) {
                 const variantAttribute = node.attributes.find(
-                  (attr: { name: { name: string } }) =>
+                  (attr) =>
+                    attr.type === "JSXAttribute" &&
                     attr.name?.name === "variant"
                 );
 
