fedbf447f3552c1a854cae6f57700968f8e01d53
adamviktora <viktora.adam@gmail.com>
2024-03-12 11:58:23 +0100

    feat(LoginMainFooterLinksItem): codemod done

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/loginMainFooterLinksItemStructureUpdated/loginMainFooterLinksItemStructureUpdatedOutput.tsx
@@ -0,0 +1,14 @@
+import { LoginMainFooterLinksItem } from "@patternfly/react-core";
+
+export const LoginMainFooterLinksItemStructureUpdatedInput = () => (
+  <LoginMainFooterLinksItem data-codemods="true">
+    <Button
+      variant="link"
+      component="a"
+      href="https://github.com/login"
+      {...{ "aria-label": "Login with Github" }}
+    >
+      <i>GitHub icon</i>
+    </Button>
+  </LoginMainFooterLinksItem>
+);
