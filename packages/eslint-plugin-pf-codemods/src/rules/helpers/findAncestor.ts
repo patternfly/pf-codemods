b3eb4f00434451bf6a405f00af108f2f8844ade4
Eric Olkowski <thatblindgeye@gmail.com>
2024-03-20 09:08:27 -0400

    Removed comments

--- packages/eslint-plugin-pf-codemods/src/rules/helpers/findAncestor.ts
+++ packages/eslint-plugin-pf-codemods/src/rules/helpers/findAncestor.ts
@@ -22,32 +22,3 @@ export function findAncestor(
     current = current.parent;
   }
 }
-
-// import { JSXElement, JSXOpeningElement } from "estree-jsx";
-
-// type ElementWithParent = (JSXOpeningElement | JSXElement) & {
-//   parent?: ElementWithParent | null;
-// };
-
-// export function findAncestorByName(
-//   node: ElementWithParent,
-//   ancestorNameToFind: string
-// ): JSXElement | undefined {
-//   if (!node) {
-//     return;
-//   }
-
-//   let current = node.parent;
-
-//   while (current) {
-//     if (
-//       current.type === "JSXElement" &&
-//       current.openingElement.name.type === "JSXIdentifier" &&
-//       current.openingElement.name.name === ancestorNameToFind
-//     ) {
-//       return current;
-//     }
-
-//     current = current.parent;
-//   }
-// }
