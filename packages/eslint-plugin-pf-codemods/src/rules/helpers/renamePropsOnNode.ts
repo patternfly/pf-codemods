f8ee50682c4138dba39fd0bd9b19f8617fe78735
Dominik Petřík <dominik.petrik@tiscali.cz>
2024-03-26 18:19:15 +0100

    refactor

--- packages/eslint-plugin-pf-codemods/src/rules/helpers/renamePropsOnNode.ts
+++ packages/eslint-plugin-pf-codemods/src/rules/helpers/renamePropsOnNode.ts
@@ -2,34 +2,23 @@ import { Rule } from 'eslint';
 import {
   JSXOpeningElement,
   ImportSpecifier,
-  JSXIdentifier,
   JSXAttribute,
+  JSXIdentifier,
 } from 'estree-jsx';
+import { renameSinglePropOnNode } from './renameSinglePropOnNode';
 
-interface RenameConfig {
+export interface RenameConfig {
   newName: string;
   message?: string | ((node: any) => string);
   replace?: boolean;
 }
 
-interface ComponentRenames {
+export interface ComponentRenames {
   [propName: string]: RenameConfig | string | Record<PropertyKey, never>;
 }
 
 export interface Renames {
   [componentName: string]: ComponentRenames;
-}
-
-function isRenameConfig(obj: any): obj is RenameConfig {
-  return (
-    typeof obj === 'object' &&
-    obj !== null &&
-    typeof obj.newName === 'string' &&
-    (obj.message === undefined ||
-      typeof obj.message === 'string' ||
-      typeof obj.message === 'function') &&
-    (obj.replace === undefined || typeof obj.replace === 'boolean')
-  );
 }
 
 export function renamePropsOnNode(
@@ -38,71 +27,28 @@ export function renamePropsOnNode(
   node: JSXOpeningElement,
   renames: Renames
 ) {
-  node.name = node.name as JSXIdentifier;
   const componentName = imports.find(
     (imp) =>
       node.name.type === 'JSXIdentifier' && imp.local.name === node.name.name
   )?.imported.name;
 
-  if (componentName) {
-    const renamedProps = renames[componentName];
-    node.attributes
-      .filter(
-        (attribute) =>
-          attribute.type === 'JSXAttribute' &&
-          attribute.name.type === 'JSXIdentifier' &&
-          renamedProps.hasOwnProperty(attribute.name?.name)
-      )
-      .forEach((attribute) => {
-        attribute = attribute as JSXAttribute;
-        attribute.name = attribute.name as JSXIdentifier;
-        const newPropObject = renamedProps[attribute.name.name];
+  if (!componentName) return;
 
-        const message =
-          isRenameConfig(newPropObject) && newPropObject.message
-            ? newPropObject.message instanceof Function
-              ? newPropObject.message(node)
-              : newPropObject.message
-            : undefined;
+  const renamedProps = renames[componentName];
 
-        if (
-          (isRenameConfig(newPropObject) && newPropObject.newName !== '') ||
-          (typeof newPropObject === 'string' && newPropObject !== '')
-        ) {
-          const newName = isRenameConfig(newPropObject)
-            ? newPropObject.newName
-            : newPropObject;
-          context.report({
-            node,
-            message:
-              message ||
-              `${attribute.name.name} prop for ${
-                (node.name as JSXIdentifier).name
-              } has been ${
-                newPropObject.replace ? 'replaced with' : 'renamed to'
-              } ${newName}`,
-            fix(fixer) {
-              return fixer.replaceText(
-                newPropObject.replace
-                  ? attribute
-                  : (attribute as JSXAttribute).name,
-                newName
-              );
-            },
-          });
-        } else {
-          context.report({
-            node,
-            message:
-              message ||
-              `${attribute.name.name} prop for ${
-                (node.name as JSXIdentifier).name
-              } has been removed`,
-            fix(fixer) {
-              return fixer.replaceText(attribute, '');
-            },
-          });
-        }
-      });
-  }
+  const JSXAttributes = node.attributes.filter(
+    (attribute) =>
+      attribute.type === 'JSXAttribute' &&
+      attribute.name.type === 'JSXIdentifier' &&
+      renamedProps.hasOwnProperty(attribute.name?.name)
+  ) as JSXAttribute[];
+
+  JSXAttributes.forEach((attribute) =>
+    renameSinglePropOnNode(
+      context,
+      attribute,
+      node,
+      renamedProps[(attribute.name as JSXIdentifier).name]
+    )
+  );
 }
