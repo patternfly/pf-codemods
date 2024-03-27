a58b56d0973f646d1f566c85cc2d344d88080773
adamviktora <84135613+adamviktora@users.noreply.github.com>
2024-03-27 20:19:29 +0100

    feat(FormFiledGroupHeaderTitleTextObject): interface renamed (typo) (#610)

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/formFiledGroupHeaderTitleTextObjectRenamed/formFiledGroupHeaderTitleTextObject-renamed.test.ts
@@ -0,0 +1,43 @@
+const ruleTester = require("../../ruletester");
+import * as rule from "./formFiledGroupHeaderTitleTextObject-renamed";
+
+const errorMessage =
+  "There was a typo in FormFiledGroupHeaderTitleTextObject interface. It was renamed to the intended FormFieldGroupHeaderTitleTextObject.";
+
+ruleTester.run("formFiledGroupHeaderTitleTextObject-renamed", rule, {
+  valid: [
+    {
+      code: `const titleTextObject: FormFiledGroupHeaderTitleTextObject = {text: "Some title", id: "form-field-group-header-title-text"};`,
+    },
+  ],
+  invalid: [
+    {
+      code: `import { FormFiledGroupHeaderTitleTextObject } from '@patternfly/react-core'; const titleTextObject: FormFiledGroupHeaderTitleTextObject = {text: "Some title", id: "form-field-group-header-title-text"};`,
+      output: `import { FormFieldGroupHeaderTitleTextObject } from '@patternfly/react-core'; const titleTextObject: FormFieldGroupHeaderTitleTextObject = {text: "Some title", id: "form-field-group-header-title-text"};`,
+      errors: [
+        {
+          message: errorMessage,
+          type: "ImportSpecifier",
+        },
+        {
+          message: errorMessage,
+          type: "TSTypeReference",
+        },
+      ],
+    },
+    {
+      code: `import { FormFiledGroupHeaderTitleTextObject } from '@patternfly/react-core'; interface MyExtension extends FormFiledGroupHeaderTitleTextObject {}`,
+      output: `import { FormFieldGroupHeaderTitleTextObject } from '@patternfly/react-core'; interface MyExtension extends FormFieldGroupHeaderTitleTextObject {}`,
+      errors: [
+        {
+          message: errorMessage,
+          type: "ImportSpecifier",
+        },
+        {
+          message: errorMessage,
+          type: "TSInterfaceHeritage",
+        },
+      ],
+    },
+  ],
+});
