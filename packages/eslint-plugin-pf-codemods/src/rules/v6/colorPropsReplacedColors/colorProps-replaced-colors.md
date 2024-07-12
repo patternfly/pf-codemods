4e69fdbb7ba4a58f73b11d8a9fc6ea4f04d53f55
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2024-07-12 11:10:38 -0400

    feat(Banner,Label): renamed cyan/gold color values (#691)

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/colorPropsReplacedColors/colorProps-replaced-colors.md
@@ -0,0 +1,17 @@
+### colorProps-replaced-colors [(#10661)](https://github.com/patternfly/patternfly-react/pull/10661)
+
+The `color` prop on Banner and Label has been updated to replace "cyan" with "teal" and "gold" with "yellow".
+
+#### Examples
+
+In:
+
+```jsx
+%inputExample%
+```
+
+Out:
+
+```jsx
+%outputExample%
+```
---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/colorPropsReplacedColors/colorProps-replaced-colors.ts
@@ -0,0 +1,54 @@
+import { Rule } from "eslint";
+import { JSXOpeningElement } from "estree-jsx";
+import { getFromPackage, getAttribute, getAttributeValue } from "../../helpers";
+
+const replacementMap: { [key: string]: string } = {
+  cyan: "teal",
+  gold: "yellow",
+};
+
+// https://github.com/patternfly/patternfly-react/pull/10661
+module.exports = {
+  meta: { fixable: "code" },
+  create: function (context: Rule.RuleContext) {
+    const { imports } = getFromPackage(context, "@patternfly/react-core");
+
+    const componentImports = imports.filter((specifier) =>
+      ["Banner", "Label"].includes(specifier.imported.name)
+    );
+
+    return !componentImports.length
+      ? {}
+      : {
+          JSXOpeningElement(node: JSXOpeningElement) {
+            if (
+              node.name.type === "JSXIdentifier" &&
+              componentImports
+                .map((imp) => imp.local.name)
+                .includes(node.name.name)
+            ) {
+              const colorProp = getAttribute(node, "color");
+              if (!colorProp) {
+                return;
+              }
+
+              const colorValue = getAttributeValue(context, colorProp.value);
+              if (Object.keys(replacementMap).includes(colorValue)) {
+                const newColorValue = replacementMap[colorValue];
+                const message = `The \`color\` prop on ${node.name.name} has been updated to replace "${colorValue}" with "${newColorValue}".`;
+                context.report({
+                  node,
+                  message,
+                  fix(fixer) {
+                    return fixer.replaceText(
+                      colorProp,
+                      `color="${newColorValue}"`
+                    );
+                  },
+                });
+              }
+            }
+          },
+        };
+  },
+};
