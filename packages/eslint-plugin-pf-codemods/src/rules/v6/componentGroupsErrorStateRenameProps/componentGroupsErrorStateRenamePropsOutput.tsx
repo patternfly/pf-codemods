4f7a8672b334d9959d4e1ce9e930a29d892d3815
adamviktora <84135613+adamviktora@users.noreply.github.com>
2024-07-25 22:20:56 +0200

    feat(ErrorState componentGroup): rename props (#711)

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/componentGroupsErrorStateRenameProps/componentGroupsErrorStateRenamePropsOutput.tsx
@@ -0,0 +1,9 @@
+import { ErrorState } from "@patternfly/react-component-groups";
+
+export const ComponentGroupsErrorStateRenamePropsInput = () => (
+  <ErrorState
+    titleText="Sample error title"
+    bodyText="Sample error description"
+    defaultBodyText="Sample default error description"
+  />
+);
