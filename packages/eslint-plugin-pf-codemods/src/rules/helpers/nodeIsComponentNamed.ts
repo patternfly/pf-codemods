f4b868dbaf3dc462deb324521187feb6a6462fa2
adamviktora <viktora.adam@gmail.com>
2024-08-21 16:21:37 +0200

    refactor: get rid of nodeIsComponentNamed helper

--- packages/eslint-plugin-pf-codemods/src/rules/helpers/nodeIsComponentNamed.ts
+++
@@ -1,9 +0,0 @@
-import { JSXElement } from "estree-jsx";
-
-export function nodeIsComponentNamed(node: JSXElement, componentName: string) {
-  if (node.openingElement.name.type === "JSXIdentifier") {
-    return node.openingElement.name.name === componentName;
-  }
-
-  return false;
-}
