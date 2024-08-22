1e685a21fb04bc1e6adad4e8baa6458be4cf9478
adamviktora <84135613+adamviktora@users.noreply.github.com>
2024-08-22 16:30:04 +0200

    feat(tokens): warn about old react-tokens usage (#739)
    * feat(tokens): warn about old react-tokens usage
    
    * fix: description link
    
    Co-authored-by: Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/tokensWarn/tokensWarnOutput.tsx
@@ -0,0 +1,5 @@
+import global_warning_color_100 from "@patternfly/react-tokens/dist/esm/global_warning_color_100";
+import { c_alert__FontSize } from "@patternfly/react-tokens";
+
+global_warning_color_100;
+c_alert__FontSize;
