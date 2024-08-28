6a2f521f4871fd2ed41a751fe10cd73d2b4db792
adamviktora <viktora.adam@gmail.com>
2024-08-28 10:56:31 +0200

    fix: update output file to not use auto-formatter

--- packages/eslint-plugin-pf-codemods/src/rules/v6/dataCodemodsCleanup/dataCodemodsCleanupOutput.tsx
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/dataCodemodsCleanup/dataCodemodsCleanupOutput.tsx
@@ -1,5 +1,5 @@
 import {
-  DualListSelector,
+  DualListSelector ,
   LoginMainFooterLinksItem,
   MastheadLogo,
 } from "@patternfly/react-core";
@@ -7,7 +7,7 @@ import {
 export const DataCodemodsCleanupInput = () => (
   <>
     <DualListSelector />
-    <LoginMainFooterLinksItem />
-    <MastheadLogo />
+    <LoginMainFooterLinksItem  />
+    <MastheadLogo  />
   </>
 );
