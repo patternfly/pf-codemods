f0eeac5880f2002d659a73cd84d9ee5a5b6ce86b
adamviktora <84135613+adamviktora@users.noreply.github.com>
2024-09-04 20:29:54 +0200

    feat(ToolbarItem): remove widths prop (#758)

--- packages/eslint-plugin-pf-codemods/src/rules/v6/toolbarRemoveProps/toolbarRemovePropsInput.tsx
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/toolbarRemoveProps/toolbarRemovePropsInput.tsx
@@ -1,6 +1,7 @@
 import {
   Toolbar,
   ToolbarContent,
+  ToolbarItem,
   ToolbarToggleGroup,
 } from "@patternfly/react-core";
 
@@ -8,6 +9,7 @@ export const ToolbarRemovePropsInput = () => (
   <>
     <Toolbar usePageInsets />
     <ToolbarContent alignSelf={{}} />
+    <ToolbarItem widths={{}} />
     <ToolbarToggleGroup alignment={{}} />
   </>
 );
