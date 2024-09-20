acc5f131df6d165828e211dcc3dc7899852fe6b6
adamviktora <viktora.adam@gmail.com>
2024-09-20 11:22:35 +0200

    feat(tokens): prefix with t_

---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/tokensPrefixWithT/tokens-prefix-with-t.ts
@@ -0,0 +1,134 @@
+import { Rule } from "eslint";
+import {
+  IdentifierWithParent,
+  ImportDefaultSpecifierWithParent,
+  ImportSpecifierWithParent,
+  getDefaultImportsFromPackage,
+  getFromPackage,
+  getImportPath,
+} from "../../helpers";
+import { Identifier, ImportSpecifier } from "estree-jsx";
+import { tTokens } from "../../../tokenLists";
+
+module.exports = {
+  meta: { fixable: "code" },
+  create: function (context: Rule.RuleContext) {
+    const tokensPackage = "@patternfly/react-tokens";
+
+    const { imports: tokenSpecifiers } = getFromPackage(context, tokensPackage);
+
+    const defaultTokenSpecifiers = getDefaultImportsFromPackage(
+      context,
+      tokensPackage
+    )
+      .map((specifier) => ({
+        specifier,
+        path: getImportPath(specifier),
+      }))
+      .filter(({ path }) => path !== undefined)
+      .map(({ specifier, path }) => ({
+        specifier,
+        token: (path as string).split("/").pop() as string,
+      }));
+
+    const getTokenPathFix = (
+      fixer: Rule.RuleFixer,
+      node: ImportSpecifierWithParent | ImportDefaultSpecifierWithParent,
+      oldToken: string,
+      newToken: string
+    ) => {
+      const packagePath = getImportPath(node);
+      if (packagePath && packagePath.includes(oldToken)) {
+        const newPath = packagePath.replace(oldToken, newToken);
+        return fixer.replaceText(node.parent?.source!, `"${newPath}"`);
+      }
+    };
+
+    const replaceToken = (
+      node:
+        | ImportSpecifierWithParent
+        | ImportDefaultSpecifierWithParent
+        | Identifier,
+      oldToken: string
+    ) => {
+      const newToken = `t_${oldToken}`;
+
+      context.report({
+        node,
+        message: `React tokens, whose value is a Patternfly token variable (has prefix --pf-t), are now prefixed with t_`,
+        fix(fixer) {
+          if (node.type === "Identifier") {
+            return fixer.replaceText(node, newToken);
+          }
+
+          const tokenPathFix = getTokenPathFix(fixer, node, oldToken, newToken);
+
+          return [
+            fixer.replaceText(
+              node.type === "ImportSpecifier" ? node.imported : node,
+              newToken
+            ),
+            ...(tokenPathFix ? [tokenPathFix] : []),
+          ];
+        },
+      });
+    };
+
+    return {
+      ImportSpecifier(node: ImportSpecifier) {
+        if (tokenSpecifiers.includes(node)) {
+          const token = node.imported.name;
+          if (tTokens.includes(token)) {
+            replaceToken(node, token);
+          }
+        }
+      },
+      ImportDefaultSpecifier(node: ImportDefaultSpecifierWithParent) {
+        const specifierWithToken = defaultTokenSpecifiers.find(
+          ({ specifier }) => node === specifier
+        );
+
+        if (!specifierWithToken) {
+          return;
+        }
+
+        const { token } = specifierWithToken;
+
+        if (tTokens.includes(token)) {
+          replaceToken(node, token);
+        }
+      },
+      Identifier(node: Identifier) {
+        const parentType = (node as IdentifierWithParent).parent?.type;
+        // handle ImportSpecifier and ImportDefaultSpecifier separately
+        if (
+          parentType === "ImportSpecifier" ||
+          parentType === "ImportDefaultSpecifier"
+        ) {
+          return;
+        }
+
+        const specifierWithToken = defaultTokenSpecifiers.find(
+          ({ specifier }) => node.name === specifier.local.name
+        );
+
+        if (specifierWithToken) {
+          const { token } = specifierWithToken;
+          if (tTokens.includes(token)) {
+            replaceToken(node, token);
+          }
+        }
+
+        const unaliasedTokenSpecifier = tokenSpecifiers.find(
+          (specifier) =>
+            specifier.local.name === specifier.imported.name &&
+            node.name === specifier.local.name
+        );
+
+        if (unaliasedTokenSpecifier && tTokens.includes(node.name)) {
+          replaceToken(node, node.name);
+        }
+      },
+    };
+  },
+};
---
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/tokensPrefixWithT/tokensPrefixWithTInput.tsx
@@ -0,0 +1,3 @@
+import color_green_10 from "@patternfly/react-tokens/dist/esm/color_green_10";
+
+color_green_10;
