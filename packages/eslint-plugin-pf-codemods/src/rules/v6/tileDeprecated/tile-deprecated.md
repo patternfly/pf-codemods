3b54b46003212c3728780451f6fcef1f79968c50
Katie McFaul <kmcfaul@redhat.com>
2024-09-24 11:26:23 -0400

    update md, add more tests

--- packages/eslint-plugin-pf-codemods/src/rules/v6/tileDeprecated/tile-deprecated.md
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/tileDeprecated/tile-deprecated.md
@@ -1,6 +1,6 @@
-### tile-deprecated [(#)](https://github.com/patternfly/patternfly-react/pull/10821)
+### tile-deprecated [(#10821)](https://github.com/patternfly/patternfly-react/pull/10821)
 
-Tile has been deprecated. Running the fix flag will update your imports to our deprecated package, but we suggest using Card instead.
+Tile has been deprecated. Running the fix flag will update your imports to our deprecated package, but we suggest using Card instead. There is a new Card example on our documentation showcasing how to set up a Card as a Tile.
 
 #### Examples
 
--- packages/eslint-plugin-pf-codemods/src/rules/v6/tileDeprecated/tile-deprecated.test.ts
+++ packages/eslint-plugin-pf-codemods/src/rules/v6/tileDeprecated/tile-deprecated.test.ts
@@ -18,5 +18,45 @@ ruleTester.run('tile-deprecated', rule, {
         },
       ],
     },
+    {
+      code: `import { Tile as CustomTile } from '@patternfly/react-core';`,
+      output: `import {\n\tTile as CustomTile\n} from '@patternfly/react-core/deprecated';`,
+      errors: [
+        {
+          message: `Tile has been deprecated. Running the fix flag will update your imports to our deprecated package, but we suggest using Card instead.`,
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: `import { Tile } from '@patternfly/react-core/dist/esm/components/Tile/index.js';`,
+      output: `import {\n\tTile\n} from '@patternfly/react-core/dist/esm/deprecated/components/Tile/index.js';`,
+      errors: [
+        {
+          message: `Tile has been deprecated. Running the fix flag will update your imports to our deprecated package, but we suggest using Card instead.`,
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: `import { Tile } from '@patternfly/react-core/dist/js/components/Tile/index.js';`,
+      output: `import {\n\tTile\n} from '@patternfly/react-core/deprecated';`,
+      errors: [
+        {
+          message: `Tile has been deprecated. Running the fix flag will update your imports to our deprecated package, but we suggest using Card instead.`,
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: `import { Tile } from '@patternfly/react-core/dist/dynamic/components/Tile/index.js';`,
+      output: `import {\n\tTile\n} from '@patternfly/react-core/deprecated';`,
+      errors: [
+        {
+          message: `Tile has been deprecated. Running the fix flag will update your imports to our deprecated package, but we suggest using Card instead.`,
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
   ],
 });
